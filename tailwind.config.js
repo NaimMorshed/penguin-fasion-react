87434e31fd10fa1e6e1a5d95e5e25b8453a73e2a
Naim Morshed <morshed.naim13@gmail.com>
2022-02-14 23:12:44 -0800

    navbar done

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
         'secondary': '#0d1137',
         'primary': '#e52165'
       },
+      textColor: {
+        'secondary': '#0d1137',
+        'primary': '#e52165'
+      }
     },
   },
   plugins: [],
